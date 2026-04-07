0d2a5dad1320b05b832307ff8f5f7c0e44221dd6
Balaji Patil <balajipatil3252@gmail.com>
2026-04-07 22:43:25 +0530

    updates

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
-import baluRoutes from "./routes/balu.route.js";
-import messageRoutes from "./routes/message.route.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 
+const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 3000;
 
-app.use("/api/balu", baluRoutes)
-app.use("/api/messages", messageRoutes)
+// =====  Routes  =====
+import authRoutes from "./routes/auth.js";
+app.use("/api/auth", authRoutes);
 
+import messageRoutes from "./routes/message.js";
+app.use("/api/message", messageRoutes);
 
-app.listen(PORT, ()=> console.log("Server running on port:",PORT));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+}
+
+app.listen(PORT, () =>
+  console.log(`Server running on port: http://localhost:${PORT}`),
+);
